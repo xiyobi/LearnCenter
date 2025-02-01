20e53eccc31c52833b941dda263cf12fab7fc6f9
Shehroz <xiyobidasturchi0011@gmail.com>
2025-02-01 11:33:38 +0500

    Add Midlleware fot rolls

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -1,8 +1,14 @@
 <?php
-
 namespace App\Http\Middleware;
 
-class RoleMiddleware
-{
+session_start();
 
+class RoleMiddleware {
+    public static function check($role): void
+    {
+        if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== $role) {
+//            header("Location: /unauthorized.php");
+//            exit();
+        }
+    }
 }
