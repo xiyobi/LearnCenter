f5666883ad0ff0c5651960b4a43988a0717869db
Shehroz <xiyobidasturchi0011@gmail.com>
2025-02-01 11:39:52 +0500

    PATCH

--- config/middleware.php
+++ config/middleware.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Middleware\RoleMiddleware;
 use Src\middleware\AuthMiddleware;
+
 
 
 
 return[
     'auth:api'=>AuthMiddleware::class,
+//    'auth' => RoleMiddleware::class
 ];
+
