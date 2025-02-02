879357bb1627b08a73ed1215fb98e8db339e457b
Shehroz <xiyobidasturchi0011@gmail.com>
2025-02-02 21:58:25 +0500

    admin and teacher get tooken

--- app/Traits/HasApiTokens.php
+++ app/Traits/HasApiTokens.php
@@ -12,7 +12,9 @@ trait HasApiTokens
     /**
      * @throws RandomException
      */
-    public function  createApiToken(int $userId): string
+
+    //user create token
+    public function  userCreateApiToken(int $userId): string
     {
         $query = "Insert into  user_api_tokens(user_id,token,expires_at,created_at)
             values(:userId, :token, :expiresAt,NOW())";
@@ -26,6 +28,49 @@ trait HasApiTokens
             ':token' => $this->api_token,
             ':expiresAt' => $this->duration,
         ]);
+        return $this->api_token;
+    }
+
+    /**
+     * @throws RandomException
+     */
+// admin create token
+    public function  adminCreateApiToken(int $adminId): string
+    {
+        $query = "Insert into  admin_api_tokens(admin_id,token,expires_at,created_at)
+            values(:adminId, :token, :expiresAt,NOW())";
+
+        $this->api_token=bin2hex(random_bytes(50));
+
+        $this->duration = date('Y-m-d H:i:s', strtotime('+' . $_ENV['API_TOKEN_EXPIRATION_YEARS'] . 'year', time()));
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([
+            ':adminId' => $adminId,
+            ':token' => $this->api_token,
+            ':expiresAt' => $this->duration,
+        ]);
+        return $this->api_token;
+    }
+    //Teacher
+
+    /**
+     * @throws RandomException
+     */
+    public function  teacherCreateApiToken(int $teacherId): string
+    {
+        $query = "Insert into  teacher_api_tokens(teacher_id,token,expires_at,created_at)
+            values(:teacherId, :token, :expiresAt,NOW())";
+
+        $this->api_token=bin2hex(random_bytes(40));
+
+        $this->duration = date('Y-m-d H:i:s', strtotime('+' . $_ENV['API_TOKEN_EXPIRATION_MONTHS'] . 'months', time()));
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([
+            ':teacherId' => $teacherId,
+            ':token' => $this->api_token,
+            ':expiresAt' => $this->duration,
+        ]);
+
         return $this->api_token;
     }
 }
